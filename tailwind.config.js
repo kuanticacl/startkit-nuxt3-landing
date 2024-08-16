227f1458af27b617be422ef8194312ad08c9b65c
Esteban Cortés R <esteban@kuantica.cl>
2024-08-16 16:34:23 -0400

    config proyect

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
